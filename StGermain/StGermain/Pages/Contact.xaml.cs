d63f0e357beee79574a17b9c935c2c4a05c7cfa4
Francis Tekobou Feudjio <francis.tekobou-feudjio@expaceo.com>
2025-11-20 15:07:21 +0100

    debut ajout page contact

--- StGermain/StGermain/Pages/Contact.xaml.cs
+++ StGermain/StGermain/Pages/Contact.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -17,6 +18,29 @@ namespace StGermain.Pages
         public Contact()
         {
             this.InitializeComponent();
+
+            AddMaps();
+            contactWebBrowser.Navigate(new Uri("https://www.google.com/maps/@48.8486087,2.2203844,2088m"));
+        }
+
+        private void AddMaps()
+        {
+            string htmlContent = @"
+                                    <html>
+                                      <head>
+                                        <link rel='stylesheet' href='https://unpkg.com/leaflet/dist/leaflet.css'/>
+                                        <script src='https://unpkg.com/leaflet/dist/leaflet.js'></script>
+                                      </head>
+                                      <body>
+                                        <div id='map' style='width: 100%; height: 100%;'></div>
+                                        <script>
+                                          var map = L.map('map').setView([48.8584, 2.2945], 14);
+                                          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+                                          L.marker([48.8584, 2.2945]).addTo(map);
+                                        </script>
+                                      </body>
+                                    </html>";
+            contactWebBrowser.NavigateToString(@"http://www.google.com");
         }
 
         // Executes when the user navigates to this page.
