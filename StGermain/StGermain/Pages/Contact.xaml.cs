aefb35209d3b72ecff60ce97b018c2a81e40610a
Francis Tekobou Feudjio <francis.tekobou-feudjio@expaceo.com>
2025-11-21 09:46:04 +0100

    premiere tentative d'ajout de Map

--- StGermain/StGermain/Pages/Contact.xaml.cs
+++ StGermain/StGermain/Pages/Contact.xaml.cs
@@ -20,7 +20,7 @@ namespace StGermain.Pages
             this.InitializeComponent();
 
             AddMaps();
-            contactWebBrowser.Navigate(new Uri("https://www.google.com/maps/@48.8486087,2.2203844,2088m"));
+            //contactWebBrowser.Navigate(new Uri("https://www.google.com/maps/@48.8486087,2.2203844,2088m"));
         }
 
         private void AddMaps()
@@ -28,19 +28,21 @@ namespace StGermain.Pages
             string htmlContent = @"
                                     <html>
                                       <head>
-                                        <link rel='stylesheet' href='https://unpkg.com/leaflet/dist/leaflet.css'/>
-                                        <script src='https://unpkg.com/leaflet/dist/leaflet.js'></script>
+                                        <link rel=""stylesheet"" href=""https://unpkg.com/leaflet/dist/leaflet.css""/>
+                                        <script src=""https://unpkg.com/leaflet/dist/leaflet.js""></script>
                                       </head>
-                                      <body>
-                                        <div id='map' style='width: 100%; height: 100%;'></div>
+                                      <body style=""color:rgb(0,0,0);background-color:rgb(0,0,0);"">
+                                        <div id=""map"" style=""width: 100%; height: 100%;""></div>
                                         <script>
-                                          var map = L.map('map').setView([48.8584, 2.2945], 14);
-                                          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+                                          var map = L.map(""map"").setView([48.8584, 2.2945], 14);
+                                          L.tileLayer(""https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"").addTo(map);
                                           L.marker([48.8584, 2.2945]).addTo(map);
                                         </script>
+                                        <script src=""https://code.jquery.com/jquery-3.7.0.min.js""></script>
+                                        <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js""></script>
                                       </body>
                                     </html>";
-            contactWebBrowser.NavigateToString(@"http://www.google.com");
+            contactWebBrowser.NavigateToString(htmlContent);
         }
 
         // Executes when the user navigates to this page.
