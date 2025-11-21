a9994ff70ba4de2f452829d57f00ce23154ffbec
Francis Tekobou Feudjio <francis.tekobou-feudjio@expaceo.com>
2025-11-21 14:26:51 +0100

    fin page contact

--- StGermain/StGermain/Pages/Contact.xaml.cs
+++ StGermain/StGermain/Pages/Contact.xaml.cs
@@ -18,7 +18,9 @@ namespace StGermain.Pages
         public Contact()
         {
             this.InitializeComponent();
-
+            //string mapIframe = "<iframe width='100%' height='100%' " +
+            //           "src='https://www.google.com/maps/embed?pb=...'></iframe>";
+            //contactWebBrowser. = mapIframe;
             AddMaps();
             //contactWebBrowser.Navigate(new Uri("https://www.google.com/maps/@48.8486087,2.2203844,2088m"));
         }
@@ -26,20 +28,17 @@ namespace StGermain.Pages
         private void AddMaps()
         {
             string htmlContent = @"
-                                    <html>
+                                   <html>
                                       <head>
-                                        <link rel=""stylesheet"" href=""https://unpkg.com/leaflet/dist/leaflet.css""/>
-                                        <script src=""https://unpkg.com/leaflet/dist/leaflet.js""></script>
                                       </head>
-                                      <body style=""color:rgb(0,0,0);background-color:rgb(0,0,0);"">
-                                        <div id=""map"" style=""width: 100%; height: 100%;""></div>
-                                        <script>
-                                          var map = L.map(""map"").setView([48.8584, 2.2945], 14);
-                                          L.tileLayer(""https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"").addTo(map);
-                                          L.marker([48.8584, 2.2945]).addTo(map);
-                                        </script>
-                                        <script src=""https://code.jquery.com/jquery-3.7.0.min.js""></script>
-                                        <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js""></script>
+                                      <body>
+                                        <iframe
+                                     		id=""inlineFrameExample""
+                                     		title=""Inline Frame Example""
+                                     		width=""350""
+                                     		height=""550""
+                                     		src=""https://www.openstreetmap.org/export/embed.html?bbox=11.5458%2C3.9333%2C11.5678%2C3.9553&amp;layer=mapnik&amp;marker=3.9443574%2C11.5568169"">
+                                          </iframe>
                                       </body>
                                     </html>";
             contactWebBrowser.NavigateToString(htmlContent);
